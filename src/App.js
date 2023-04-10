7ab5d7c248be1a3edc2e5fffa9448fcbb7dac6f1
ElviraTsilo <56411952+ElviraTsilo@users.noreply.github.com>
2023-04-10 16:41:34 +0300

    Created UI for currency converter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Currency Converter</h1>
+      <h2>Check live foreign currency exchange rates</h2>
+      <div className="container">
+        <div className="row row-of-labels">
+          <div className="col-4">
+            <label htmlFor="amount" id="amount">Amount</label>
+          </div>
+          <div className="col-4">
+            <label htmlFor="from" id="from">From</label>
+          </div>
+          <div className="col-4">
+            <label htmlFor="to" id="to">To</label>
+          </div>
+        </div>
+        <div className="row">
+            <div className="col-4">
+                <div className="form-group form-group-warning">
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="amount"
+                    />
+                </div>
+            </div>
+            <div className="col-4">
+                <div className="form-group form-group-warning">
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="from-curensy"
+                    />
+                </div>
+            </div>
+            <div className="col-4">
+                <div className="form-group form-group-warning">
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="to-curensy"
+                    />
+                </div>
+            </div>
+        </div>
+          <button
+              type="button"
+              className="btn btn-primary"
+          >
+              Convert
+          </button>
+      </div>
     </div>
   );
 }
